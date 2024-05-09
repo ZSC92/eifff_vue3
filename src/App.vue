3e7b476466e4eb22f7e1d1cc4e0e56da4bfd049b
张杨菲 <2325798814@qq.com>
2024-05-09 14:26:59 +0800

    fix: 安装必备插件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
+<template>
+  <h1>SVG测试</h1>
+  <svg>
+    <use xlink:href="#icon-food"></use>
+  </svg>
+</template>
+
 <script setup lang="ts">
 
 </script>
-
-<template>
-  我是APP
-</template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'virtual:svg-icons-register'
+
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+  // 国际化配置
+})
+// console.log(import.meta.env)
+app.mount('#app')
